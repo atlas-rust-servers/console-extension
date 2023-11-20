ff1da70060768ab030f99e3ed27c7b81174f1e18
Ilovepatatos <gabriel@khome.org>
2023-11-20 11:31:20 -0500

    Init

---
+++ src/ConsoleExt/ConsoleExt.cs
@@ -0,0 +1,18 @@
+﻿using Oxide.Core;
+using Oxide.Core.Extensions;
+
+namespace Oxide.Ext.ConsoleExt
+{
+    public class ConsoleExt : Extension
+    {
+        private static readonly VersionNumber s_ExtensionVersion = new(1, 0, 0);
+
+        public override string Name => "ConsoleExt";
+        public override string Author => "Ilovepatatos";
+        public override VersionNumber Version => s_ExtensionVersion;
+
+        public override bool SupportsReloading => true;
+
+        public ConsoleExt(ExtensionManager manager) : base(manager) { }
+    }
+}
