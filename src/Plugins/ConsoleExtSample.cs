5e292bebbc5f58f9cfeb82adf1cdde64b46d4ee1
Ilovepatatos <gabriel@khome.org>
2023-11-25 16:14:09 -0500

    Sample script

---
+++ src/Plugins/ConsoleExtSample.cs
@@ -0,0 +1,17 @@
+﻿using JetBrains.Annotations;
+using Oxide.Core.Plugins;
+using Oxide.Ext.ConsoleExt;
+
+namespace Oxide.Plugins
+{
+    [Info("(Sample) ConsoleExt", "Ilovepatatos", "1.0.0")]
+    public class ConsoleExtSample : CovalencePlugin
+    {
+        [UsedImplicitly]
+        [HookMethod(nameof(OnServerInitialized))]
+        private void OnServerInitialized(bool initial)
+        {
+            OxideConsole.Log("[(Sample) ConsoleExt] Console works!", OxideConsole.GREEN);
+        }
+    }
+}
