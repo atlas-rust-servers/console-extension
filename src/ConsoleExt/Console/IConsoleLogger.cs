b0ce47c076a5d1b76f75c0db926a8972f394a8b0
Ilovepatatos <gabriel@khome.org>
2023-12-19 18:02:03 -0500

    Create IConsoleLogger.cs

---
+++ src/ConsoleExt/Console/IConsoleLogger.cs
@@ -0,0 +1,9 @@
+﻿using JetBrains.Annotations;
+
+namespace Oxide.Ext.ConsoleExt;
+
+[UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+public interface IConsoleLogger
+{
+    void WriteLine(object obj, string color = OxideConsole.NORMAL);
+}
