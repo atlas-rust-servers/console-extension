00aa2c56530cbc6b83b0ba4985e864bcd6b7a30b
Ilovepatatos <gabriel@khome.org>
2024-06-08 15:24:39 -0400

    Create StringEx.cs

---
+++ src/ConsoleExt/Extensions/StringEx.cs
@@ -0,0 +1,12 @@
+﻿using JetBrains.Annotations;
+
+namespace Oxide.Ext.ConsoleExt;
+
+[UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+public static class StringEx
+{
+    public static string ToColor(this string value, string color)
+    {
+        return string.Format(color, value);
+    }
+}
