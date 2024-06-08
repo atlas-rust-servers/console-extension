4534ef64b044b80298f1345139f5a52cd32d672b
Ilovepatatos <gabriel@khome.org>
2024-06-08 15:29:33 -0400

    Create StringBuilderEx.cs

---
+++ src/ConsoleExt/Extensions/StringBuilderEx.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+using JetBrains.Annotations;
+
+namespace Oxide.Ext.ConsoleExt;
+
+[UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+public static class StringBuilderEx
+{
+    public static void Append(this StringBuilder sb, string value, string color)
+    {
+        sb.Append(value.ToColor(color));
+    }
+
+    public static void AppendLine(this StringBuilder sb, string value, string color)
+    {
+        sb.AppendLine(value.ToColor(color));
+    }
+}
